9a44fe90391000042ff8f252339e07128f68d791
POOSTOTEL <sashamix101@mail.ru>
2023-06-18 18:28:05 +0300

    Homework task 4

--- src/homework3/Task4.java
+++ src/homework3/Task4.java
@@ -1,4 +1,10 @@
 package homework3;
 
 public class Task4 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 25) {
+            System.out.print(++i + " ");
+        }
+    }
 }
