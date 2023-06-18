58b8d37e84f71f926cb1a5632267c3ecaa8ae0e0
POOSTOTEL <sashamix101@mail.ru>
2023-06-18 18:45:08 +0300

    Homework task 7

--- src/homework3/Task7.java
+++ src/homework3/Task7.java
@@ -1,4 +1,23 @@
 package homework3;
 
+import java.util.Scanner;
+
 public class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Input counter: ");
+        int sum = 0, counter = scanner.nextInt();
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i);
+            //Мне кажется, что оформление вывода можно было сделать как-то более оптимально,
+            //миновав при этом постоянные логические проверки.
+            if (counter != i) {
+                System.out.print(" + ");
+            } else {
+                System.out.print(" = ");
+            }
+            sum += i;
+        }
+        System.out.print(sum);
+    }
 }
