6ce81ee09bf38da7840726f34f8bb675918a573e
POOSTOTEL <sashamix101@mail.ru>
2023-06-18 18:19:00 +0300

    Homework task 2

--- src/homework3/Task2.java
+++ src/homework3/Task2.java
@@ -1,4 +1,19 @@
 package homework3;
 
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, write time: ");
+        int time = scanner.nextInt();
+        System.out.println();
+        if (time >= 10 && time <= 19) {
+            System.out.print("We open. Welcome!");
+        } else if ((time >= 0 && time < 10) || (time > 19 && time <= 24)) {
+            System.out.print("We closed. Try visit this shop tomorrow!");
+        } else {
+            System.out.print("Input error.");
+        }
+    }
 }
