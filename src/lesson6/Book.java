e0b6ac1885209ea4d64f4502432bbbaf716a824c
POOSTOTEL <sashamix101@mail.ru>
2023-06-26 19:45:29 +0300

    My first true class.

---
+++ src/lesson6/Book.java
@@ -0,0 +1,11 @@
+package lesson6;
+
+public class Book {
+    String name;
+    String author;
+    char firstLetterInName;
+    boolean weHasThisBoolInLibrary;
+    public Book () {
+        System.out.println("This constructor is called");
+    }
+}
