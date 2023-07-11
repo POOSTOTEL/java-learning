5326483b78e986f6b0f7ecfa50953ece1790a91c
POOSTOTEL <sashamix101@mail.ru>
2023-07-11 23:39:55 +0300

    homework 9

---
+++ src/homework9/LibraryRunner.java
@@ -0,0 +1,21 @@
+package homework9;
+
+public class LibraryRunner {
+    public static void main(String[] args) {
+        Reader[] readers = new Reader[3];
+        readers[0] = new Reader("Ivanov A. P. ", 1245);
+        readers[1] = new Reader("Petrov B. S. ", 7842);
+        readers[2] = new Reader("Pavlov U. E. ", 6478);
+        Book[] books = new Book[3];
+        books[0] = new Book("Star wars ", "Pushkin A. S. ");
+        books[1] = new Book("Cars ", "Fridrig A. P. ");
+        books[2] = new Book("War and life ", "Tolstoi H. M. ");
+        String[] booksString = {"Ferry ", "How build house "};
+        readers[0].takeBook(10);
+        readers[0].takeBook(booksString);
+        readers[0].takeBook(books);
+        readers[1].takeBook(2);
+        readers[2].takeBook("Holidays ", "Kingdoms ");
+        System.out.println(Book.getAmountBooks());
+    }
+}
---
+++ src/homework9/Reader.java
@@ -0,0 +1,34 @@
+package homework9;
+
+public class Reader {
+    String name;
+    int libraryCardNumber;
+
+    public Reader(String name, int libraryCardNumber) {
+        this.name = name;
+        this.libraryCardNumber = libraryCardNumber;
+    }
+
+    void takeBook(int amount) {
+        System.out.println(name + "взял/а " + amount);
+        Book.amountBooks += amount;
+    }
+
+    void takeBook(String... books) {
+        System.out.print(name + "взял/а: ");
+        for (String book : books) {
+            System.out.print(book + " ");
+        }
+        System.out.println();
+        Book.amountBooks += books.length;
+    }
+
+    void takeBook(Book... books) {
+        System.out.print(name + "взял/а: ");
+        for (Book book : books) {
+            System.out.print(book.getTitle() + " ");
+        }
+        System.out.println();
+        Book.amountBooks += books.length;
+    }
+}
